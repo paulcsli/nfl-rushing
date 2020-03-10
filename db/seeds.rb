c0bb42e0839fdca3cdf0028811ebc43daa49970d
Paul Li <paul.chet.sebs.li@gmail.com>
2020-03-10 18:52:56 -0400

    seeds.rb script to load db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+require 'pp'
+
+Player.delete_all
+
+begin
+  # TODO: maybe have url as an UI input
+  url = "https://raw.githubusercontent.com/tsicareers/nfl-rushing/master/rushing.json"
+  file = open(url)
+  players = JSON.load(file)
+rescue => exception
+  puts "Invalid URL or invalid json file"
+else
+  data = []
+  players.each do |player|
+    pp player
+    object = {
+      "name" => player["Player"], 
+      "team" => player["Team"], 
+      "position" => player["Pos"], 
+      "attempts_per_game" => player["Att/G"], 
+      "attempts" => player["Att"], 
+      "rushing_yds_total" => player["Yds"], 
+      "rushing_yds_per_attempt" => player["Avg"], 
+      "rushing_yds_per_game" => player["Yds/G"], 
+      "touch_down" => player["TD"], 
+      "longest_rush" => player["Lng"], 
+      "first_down" => player["1st"], 
+      "first_down_pct" => player["1st%"], 
+      "twenty_yards" => player["20+"], 
+      "forth_yards" => player["40+"], 
+      "fumbles" => player["FUM"]
+    }
+    data << object
+  end
+  Player.create!(data)
+end
